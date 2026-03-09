6f3d4f48f39ca17f5dbec22763afb6ba9c79fcef
pradeepg-21 <pradeep21.ganesh@gmail.com>
2026-03-09 07:49:19 +0530

    Fixed tile snapping issue Switched from camera snapping between tile indexes to smooth pixel based camera movement. Additional padding to tile indexes ensures partially visible tiles in the screen appear without blanking.

--- src/main/java/Map/MapRenderer.java
+++ src/main/java/Map/MapRenderer.java
@@ -8,21 +8,40 @@ import java.awt.*;
 
 public class MapRenderer {
 
+    private static final TileType[] TILE_TYPES = TileType.values();
+
     public static void renderMap(GameMap gameMap, int playerWorldX, int playerWorldY, Graphics2D g2) {
-        int [][] map = gameMap.getGameMap();
 
-        int leftLimit = playerWorldX / GameConstants.tileSize - GameConstants.maxScreenCols / 2;
-        int topLimit = playerWorldY / GameConstants.tileSize - GameConstants.maxScreenRows / 2;
+        int[][] map = gameMap.getGameMap();
 
-        leftLimit = Math.max(0, leftLimit);
-        topLimit = Math.max(0, topLimit);
+        int cameraX = playerWorldX - GameConstants.screenWidth / 2;
+        int cameraY = playerWorldY - GameConstants.screenHeight / 2;
 
-        int yRightLimit = Math.min(gameMap.getNumCols(), leftLimit + GameConstants.maxScreenCols);
-        int xBottomLimit = Math.min(gameMap.getNumRows(), topLimit + GameConstants.maxScreenRows);
-        for(int i=topLimit; i<xBottomLimit; i++) {
-            for(int j=leftLimit; j<yRightLimit; j++) {
+        // Adding extra tiles off camera to avoid blank tiles for partially visible tiles
+        int leftTileIndex = cameraX / GameConstants.tileSize - 1;
+        int topTileIndex = cameraY / GameConstants.tileSize - 1;
+
+        int rightTileIndex = leftTileIndex + GameConstants.maxScreenCols + 2;
+        int bottomTileIndex = topTileIndex + GameConstants.maxScreenRows + 2;
+
+        leftTileIndex = Math.max(0, leftTileIndex);
+        topTileIndex = Math.max(0, topTileIndex);
+
+        rightTileIndex = Math.min(gameMap.getNumCols(), rightTileIndex);
+        bottomTileIndex = Math.min(gameMap.getNumRows(), bottomTileIndex);
+
+        for(int i = topTileIndex; i < bottomTileIndex; i++) {
+            for (int j = leftTileIndex; j < rightTileIndex; j++) {
+
                 int tileId = map[i][j];
-                TileRenderer.renderTile(TileType.values()[tileId], (j - leftLimit) * GameConstants.tileSize, (i - topLimit) * GameConstants.tileSize, g2);
+
+                int worldX = j * GameConstants.tileSize;
+                int worldY = i * GameConstants.tileSize;
+
+                int screenX = worldX - cameraX;
+                int screenY = worldY - cameraY;
+
+                TileRenderer.renderTile(TILE_TYPES[tileId], screenX, screenY, g2);
             }
         }
     }
